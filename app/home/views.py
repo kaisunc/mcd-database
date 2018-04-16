69bdaa07358e603c48793682926440410623e7d9
julio <julio@mail.chungyo.net>
2018-04-16 18:03:11 +0800

    extension recognition ok. (image only). download mechanism needs work

--- app/home/views.py
+++ app/home/views.py
@@ -1,4 +1,4 @@
-
+import os
 from flask import flash, request, redirect, render_template, url_for, json
 from flask_login import login_required
 from werkzeug import secure_filename
@@ -45,27 +45,33 @@ def homepage():
 @home.route('/media', methods=['GET'])    
 @login_required
 def media():
-    #namespace = jinja2_escapejs_filter("media")
     namespace = "media"
-    #field_list = Media.query.all()[0].field_list()
-    # field_list = jinja2_escapejs_filter([{'name': 'name', 'type': 'text'}, {'name': 'category', 'type': 'select'}, {'name': 'url', 'type': 'text'}, {'name': 'tags', 'type': 'text'},{'name': 'assigned', 'type': 'select'}])
-    # hide = jinja2_escapejs_filter(['timestamp'])
-
-    categorys = Category.query.all()
-    mt = []
-    for t in categorys:
-        data = {"name": t.name, "id": t.id}
-        mt.append(data)
-
-    return render_template('home/table.html', title='Database', namespace=namespace, db_filter="fonts", menu=mt)
+    model = getModel(namespace)
+    items = model.query.all()
+    fields, columns, columnDefs = getFields(model)
+    ff = fields
+    columns = jinja2_escapejs_filter(columns)
+    columnDefs = jinja2_escapejs_filter(columnDefs)
+    fields = jinja2_escapejs_filter(fields)
+    
+    return render_template('home/table.html', title='Media', namespace=namespace, columns=columns, columnDefs=columnDefs, fields=fields, ff=ff)
 
 
 @home.route('/category', methods=['GET'])
 @login_required
 def category():
-    #namespace = jinja2_escapejs_filter("category")
     namespace = "category"
-    return render_template('home/table.html', title='Category', namespace=namespace, db_filter="")
+    model = getModel(namespace)
+    items = model.query.all()
+    fields, columns, columnDefs = getFields(model)
+
+    columns = jinja2_escapejs_filter(columns)
+    columnDefs = jinja2_escapejs_filter(columnDefs)
+    fields = jinja2_escapejs_filter(fields)
+    return render_template('home/table.html', title='Category', namespace=namespace, columns=columns, columnDefs=columnDefs, fields=fields)
+
+    #namespace = "category"
+    #return render_template('home/table.html', title='Category', namespace=namespace, db_filter="")
 
 @home.route('/user', methods=['GET'])
 @login_required
@@ -77,60 +83,18 @@ def user():
 @home.route('/upload', methods=['POST'])
 def upload():
     if request.method == 'POST':
-        base_path = r"C:\\Users\\julio\\source\\"
+        base_path = "C:/Users/julio/source"
         f = request.files['file']
+        print request
         category  = request.args.get('category')
         file_id = request.args.get('file_id')
-        file_path = "%s\\%s\\%s\\%s" % (base_path, category, file_id, f.filename)
-        print file_path
-        #f.save(file_path)
-        return 'ok'
+        folder_path = "%s/%s/%s" % (base_path, category, file_id)
 
-
-# passing data directly to html
-@home.route('/test', methods=['GET'])
-def test():
-    namespace = "media"
-    model = getModel(namespace)
-    items = model.query.all()
-    fields, columns, columnDefs = getFields(model)
-
-    dt_data = []
-    for row in items:
-        dt_data.append(row.as_dict1(fields))
-
-    dt_data = jinja2_escapejs_filter(dt_data)
-    columns = jinja2_escapejs_filter(columns)
-    columnDefs = jinja2_escapejs_filter(columnDefs)
-
-    return render_template('home/test.html', title='test', namespace=json.dumps(namespace), data=dt_data, columns=columns, columnDefs=columnDefs)
-
-# standard data_table js ajax call
-@home.route('/test_ajax', methods=['GET'])
-def test_ajax():
-    namespace = "media"
-    model = getModel(namespace)
-    items = model.query.all()
-    fields, columns, columnDefs = getFields(model)
-
-    columns = jinja2_escapejs_filter(columns)
-    columnDefs = jinja2_escapejs_filter(columnDefs)
-
-    return render_template('home/test_ajax.html', title='test', namespace=json.dumps(namespace), columns=columns, columnDefs=columnDefs)
-
-@home.route('/test_ajax_socket', methods=['GET'])
-def test_ajax_socket():
-    namespace = "media"
-    model = getModel(namespace)
-    items = model.query.all()
-    fields, columns, columnDefs = getFields(model)
-
-    columns = jinja2_escapejs_filter(columns)
-    columnDefs = jinja2_escapejs_filter(columnDefs)
-    fields = jinja2_escapejs_filter(fields)
-
-    return render_template('home/table1.html', title='test', namespace=namespace, columns=columns, columnDefs=columnDefs, fields=fields)
-
+        if os.path.isdir(folder_path) is False:
+            os.makedirs(folder_path)
+        file_path = "%s/%s" % (folder_path, f.filename)            
+        f.save(file_path)
+        return "ok"
 
 @home.route('/ajax', methods=['GET'])
 def ajax():
@@ -141,18 +105,6 @@ def ajax():
     model = getModel(namespace)
     items = model.query.all()
     fields, columns, columnDefs = getFields(model)
-
-    dt_data = []
-    for row in items:
-        dt_data.append(row.as_dict1(fields))
-    t = {"data": dt_data}
-    return json.dumps(t)
-
-@home.route('/ajax_data', methods=['GET'])
-def ajax_data():
-    namespace = "media"
-    model = getModel(namespace)
-    items = model.query.all()
 
     dt_data = []
     for row in items:
