e01d3b3edc54edcb20fd6a1c96835f83d885f1fc
julio <julio@mail.chungyo.net>
2018-05-28 17:10:17 +0800

    yeti login

--- app/auth/views.py
+++ app/auth/views.py
@@ -25,6 +25,7 @@ def register():
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+
     if form.validate_on_submit():
         user = User.query.filter_by(name=form.name.data).first()
         if user is not None and user.verify_password(form.password.data):
